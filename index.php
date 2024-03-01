8ab9b997c2ea4cd850837c36b7d631f14162adfe
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-03-01 15:05:26 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,343 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Author" content="Carrington Condos" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/silverBox.min.css">
+
+
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    <title>Carrington Condo Calgary | Coming Soon - Register Today</title>
+    <meta name="title" content="Carrington Condo Calgary | Coming Soon - Register Today">
+    <meta name="description" content="Carrington Condo Calgary, a development by Truman Homes, is an upcoming condos in Calgary, located on North West Calgary. The project is expected to be completed in 2028.">
+    <meta name="keywords" content="Carrington Condos, Carrington Condo Calgary, Condos in Calgary, Carrington Condos Truman Homes, Truman Homes">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="1 days">
+    <meta property="og:image" content="https://lincolncondo.ca/1.jpeg">
+    <meta property="og:site_name" content="Carrington Condos" />
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "Carrington Condos",
+            "url": "https://carringtoncondo.ca/"
+        }
+    </script>
+</head>
+
+<body>
+    <div class="sticky-top topp bg-white" id="header">
+        <nav class="navbar navbar-expand-md navbar-light" id="navbar">
+            <div class="container">
+                <a href="/" class="navbar-brand">
+                   <p>Carrington Condos</p>
+                </a>
+                <a href="#Contact" class="btn call-btn d-block d-md-none btn-sm py-2">
+                    Register Now
+                </a>
+                <button class="d-block d-md-none btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" fill="black" />
+                    </svg>
+                </button>
+                <div class="row me-0 row-cols-2 collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="col-12 d-flex justify-content-start justify-content-md-end">
+                        <ul class="navbar-nav mb-2 mb-lg-0 align-items-start align-items-md-center">
+                            <li class="nav-item">
+                                <a href="#hero" class="nav-link scrollto" aria-current="page">
+                                    Home
+                                </a>
+                            </li>
+                            <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#builder">Builder</a>
+                            </li>
+                            <!-- <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#features">Amenities</a>
+                            </li> -->
+                            <!-- <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#plans">Plans & Pricing</a>
+                            </li> -->
+
+                            <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#location">Location</a>
+                            </li>
+                            <!-- <li class="nav-item px-md-2">
+                                <a class="nav-link scrollto" href="#contact">Contact</a>
+                            </li> -->
+                            <li class="nav-item px-md-2 d-none d-md-block">
+                                <a href="#Contact" class="btn call-btn fss">
+                                    Register Now
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="main">
+    <section>
+        <div class="container pt-3 " data-aos="fade-up" data-aos-duration="2000" data-aos-delay="200">
+            <div class="my-md-5 my-5"></div>
+            <div class="pt-md-5 pt-5" data-aos="fade-up">
+                <h1 class=" main-titlee  pb-4 ps-3 ps-md-0 d-flex justify-content-center brand-color center-sm">
+                   Carrington Condo Calgary
+                </h1>
+                <p class="textt justify-content-center brand-color center-sm text-center pb-2 fs-4 px-2 ">
+                    <span>
+                    Carrington Condos in Calgary is a brand New Pre-construction condo development by Truman Homes, Located at North West Calgary in Calgary.
+                    </span> </p>
+            </div>
+            <div class="d-flex justify-content-center text-center textt  fs-4 px-1">
+                <div class="list d-block ">
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold">Developer:</span> Truman Homes.
+                    </p>
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold">Project Type:</span> Condominiums
+                    </p>
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold">Address:</span> North West Calgary, Calgary
+                    </p>
+                
+
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold"> Pricing: </span> Not Available
+                    </p>
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold"> Number of Units:</span> 420
+                    </p>
+                   
+                    <p class="mybot pb-md-0 pb-0 ">
+                        <span class="fw-bold"> Expected Completion: </span> Q2 2024
+                    </p>
+
+
+
+                   
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="builder">
+    <div class="pt-md-5 pt-5"></div>
+    <div class="pt-md-5 pt-5"></div>
+  <div class="pt-5">
+  <h2 class="fw-bold fs-1 mb-0 text-center justify-content-center brand-color">
+                    <span class="mx-1">About Truman Homes </span>
+                </h2>
+            
+                <span class=" pt-2 small-text text-center d-flex justify-content-center align-items-center custom-underline pb-3 textt fs-4 px-2 ">Truman Homes is Calgary's Leading New Homes Development Company. Truman is Award-winning master builder serving Calgary & area for over 35 years. For more than three and a half decades, Truman Homes has established itself as a shining example of excellence and unwavering commitment within the construction industry. Their reputation isn't limited to mere words; it's substantiated by numerous accomplishments that underscore their steadfast dedication to their craft and their customers. Truman Homes' Commitment to Quality A Legacy of Acknowledgment Truman's remarkable expertise in constructing homes has not escaped recognition.</span>
+  </div>
+    </section>
+
+    
+    
+
+    <section>
+        <div class="container pt-md-5 pt-sm-2">
+            <div class="pt-md-5 pt-5"></div>
+            <div class="py-5" id="location">
+                <h2 class="fw-bold fs-1 mb-0 text-center justify-content-center brand-color">
+                    <span class="mx-1">Carrington Condos - Location </span>
+                </h2>
+                <span class="small-text text-center d-flex justify-content-center custom-underline pb-3">Northwest Calgary, AB</span>
+                <div>
+                    <div class="bg-white p-1   rounded-mine d-flex justify-content-center">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d160296.83345661673!2d-114.33171220052482!3d51.11513842733855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537168d45bdbb7bd%3A0x97850dd7002facb0!2sNorthwest%20Calgary%2C%20Calgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2snp!4v1709283634285!5m2!1sen!2snp" width="90%" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <section>
+        <div class="pt-5" id="Contact">
+            <div class="pt-5 mt-5">
+                <div class="row mx-0 ">
+                    <h6 class="text-center fs-1 fw-bold pt-3 register"> Register today</h6>
+                    <div class="registertext px-4 d-flex justify-content-center align-items-center  text-center">
+                        <p>Register today to get the more information on Carrington Condos coming up in Calgary
+                        </p>
+                    </div>
+
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 mx-0 g-5 pb-md-5">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6 px-md-5">
+
+                        <form action="./contactForm.php" method="POST">
+                            <div class="row">
+                                <div class="mb-3"><input type="text" placeholder="Name" name="name" id="name" class="fields" required></div>
+
+                            </div>
+                            <div class="row row-cols-1 row-cols-sm-2 ">
+                                <div class="col">
+                                    <div class="mb-3 "><input type="text" aria-describedby="emailHelp" placeholder="Phone" name="phone" id="phone" class="fields" required></div>
+                                </div>
+                                <div class="col ">
+                                    <div class="mb-3"><input type="text" aria-describedby="emailHelp" placeholder="Email" name="email" id="email" class="fields" required></div>
+                                </div>
+                            </div>
+                            <div class="row me-0 row-cols-1">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <div class="form-floating">
+                                            <select class="form-select" name="realtor" id="realtor" aria-label="Floating label select example" required>
+                                                <option value="No">No</option>
+                                                <option value="Yes">Yes</option>
+                                            </select>
+                                            <label for="floatingSelect">Are you a realtor or working with one? </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class=" "><textarea name="message" id="message" placeholder="Enter your message" class="fields mess" required></textarea></div>
+                            </div>
+                            <div class="row">
+                                <p class="text-muted sm-text text-center mt-0">*I consent to receive future communications about Carrington Condos. I understand I can opt out at anytime by sending an email.</p>
+                            </div>
+                            <div class="row">
+                                <div class="col text-center">
+                                    <!-- <input type="submit" onclick="sendMail()" value="Send" class="btn call-btn btn-sm p-2 px-4 btn-lg"> -->
+                                    <Button class="btn call-btn btn-sm p-2 px-4 btn-lg">Send</Button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div class="row-cols-md-3"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="mt-5 pt-3">
+        <img loading="lazy" src="images/line.png" alt="Line image for character" class="img-fluid foot-up-img" />
+    </div>
+
+    <div class="floating fixxcont2 mb-3">
+        <a href="#Contact" class="btn bgggggggg br-m d-block d-md-none w-100 py-2 shadow-lg">Send a message  
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path><path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z">
+                </path>
+            </svg>
+            </a>
+        </div>
+    <footer class="footerr  py-5 mb-4">
+        <div class="container ">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 pt-5 justify-content-between ">
+                <div class="col-md-5 my-3  center-sm px-4 px-md-0">
+                    <p class=" ">
+                    <h1 class="navbar-brand-foot fs-4 "><a href="https://dolphy.ca/pre-construction-homes/calgary/carrington-condos" target="_blank" class="text-dark text-decoration-none">Carrington Condos</a></h1>
+                    </p>
+                    <span class="sm-text justifyy text-secondary">
+                        We do not represent the builder. Information provided on this website is gathered from publicly available websites. The information might not be updated and could include inaccurate or outdated information such as location, pricing, availability, floor plans etc. The logos, images and renderings might be protected by copyright owned by the builders.. Not intended to solicit those under contract with another brokerage. </span>
+                    <div class="list pt-md-3 pt-3 d-md-block d-none">
+                        <p class=""> <a href="https://dolphy.ca/pre-construction-homes/calgary" target="_blank" class="text-dark text-decoration-none"><strong>
+                                Check out all calgary pre construction homes projects on
+                               Dolphy.</strong></a>
+                        </p>
+
+                    </div>
+                </div>
+                <div class="col-md-5 py-md-5 py-5 center-sm px-md-5 px-5 ">
+                    <div>
+                        <h5 class="text-dark fw-bold mt-3 mb-3 ">About Carrington Condos</h5>
+                        <div class="list d-block ">
+                            <p class="mybot pb-md-0 pb-0 ">
+                                Builder : Truman Homes
+                            </p>
+
+                            <p class="mybot pb-md-0 pb-0 ">
+                                Address : Northwest Calgary, AB
+                            </p>
+                            <p class="mybot pb-md-0 pb-0 ">
+                                Building Type : Condos
+                            </p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-2 mt-md-5 pt-md-5 mt-sm-0 pt-4 center-sm d-flex  justify-content-center">
+                    <img src="images/truman_logo.png" class="img-fluid regoimg mx-3" alt="Truman Homes-logo">
+
+                </div>
+            </div>
+
+            <div class="list mt-5 pt-5 d-md-none d-block center-sm">
+                <p class="fw-bold px-2">
+                <a href="https://dolphy.ca/pre-construction-homes/calgary" target="_blank" class="text-dark text-decoration-none"> <strong>Check out all calgary pre construction homes projects on Dolphy.</strong></a>
+                </p>
+
+            </div>
+
+        </div>
+    </footer>
+    <script src="js/silverBox.min.js"></script>
+
+    <?php
+    if (
+        isset($_SESSION['success'])
+    ) {
+    ?>
+        <script type="text/javascript" async>
+            silverBox({
+                title: {
+                    text: "Success",
+                    alertIcon: "success",
+                },
+                text: "Your Message has been received.\n We'll get back to you soon.",
+            });
+        </script>
+    <?php
+        unset($_SESSION['success']);
+    }
+
+    if (
+        isset($_SESSION['error'])
+    ) {
+    ?>
+        <script type="text/javascript" async>
+            silverBox({
+                title: {
+                    text: "Error",
+                    alertIcon: "error",
+                },
+                text: "Your Message has not been received.",
+            });
+        </script>
+    <?php
+        unset($_SESSION['error']);
+    }
+    ?>
+</body>
+
+</html>
